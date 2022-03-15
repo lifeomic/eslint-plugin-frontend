1cf43c968c6c43d6601d76f8fd52724fa99fa1e2
Haley Ward <haley.ward@lifeomic.com>
2022-03-15 11:29:15 -0400

    NO-TICKET add rule to ensure formatMessage messageDescriptor is defined

---
+++ tests/lib/rules/require-react-intl-format-message-param.js
@@ -0,0 +1,46 @@
+const rule = require("../../../lib/rules/require-react-intl-format-message-param");
+const RuleTester = require("eslint").RuleTester;
+
+const eslintTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: "module" },
+});
+
+eslintTester.run("require-react-intl-format-message-params", rule, {
+  valid: [
+    `formatMessage({ id: 'foo', defaultMessage: 'foo' })`,
+  ],
+  invalid: [
+     {
+      code: `formatMessage({ id: 'foo' })`,
+      errors: [
+        {
+          message: `Message descriptor passed into formatMessage is missing "defaultMessage".`,
+        },
+      ],
+    },
+    {
+      code: `formatMessage({ defaultMessage: 'bar' })`,
+      errors: [
+        {
+          message: `Message descriptor passed into formatMessage is missing "id".`,
+        },
+      ],
+    },
+    {
+      code: `formatMessage(undefined)`,
+      errors: [
+        {
+          message: `Undefined message descriptor passed into formatMessage.`,
+        },
+      ],
+    },
+    {
+      code: `formatMessage({})`,
+      errors: [
+        {
+          message: `Undefined message descriptor passed into formatMessage.`,
+        },
+      ],
+    },
+  ],
+});
